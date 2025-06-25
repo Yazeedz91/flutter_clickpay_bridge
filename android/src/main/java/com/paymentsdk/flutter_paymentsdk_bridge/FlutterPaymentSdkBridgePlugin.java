e7a38ce4cdc77b7f53f5ba3ca488da158e135206
Yazeed Sobahi <yazeedz91@hotmail.com>
2025-06-25 14:32:20 +0300

    remove registerWith function

--- android/src/main/java/com/paymentsdk/flutter_paymentsdk_bridge/FlutterPaymentSdkBridgePlugin.java
+++ android/src/main/java/com/paymentsdk/flutter_paymentsdk_bridge/FlutterPaymentSdkBridgePlugin.java
@@ -45,7 +45,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import static com.payment.paymentsdk.integrationmodels.PaymentSdkApmsKt.createPaymentSdkApms;
 import static com.payment.paymentsdk.integrationmodels.PaymentSdkLanguageCodeKt.createPaymentSdkLanguageCode;
@@ -72,38 +71,19 @@ public class FlutterPaymentSdkBridgePlugin implements FlutterPlugin, MethodCallH
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        context = flutterPluginBinding.getApplicationContext();
-        channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutter_payment_sdk_bridge");
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_payment_sdk_bridge");
         channel.setMethodCallHandler(this);
-        EventChannel eventChannel = new EventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor().getBinaryMessenger(), streamName);
-        eventChannel.setStreamHandler(
-                new EventChannel.StreamHandler() {
-                    @Override
-                    public void onListen(Object args, final EventChannel.EventSink events) {
-//                Log.w(TAG, "adding listener");
-                        eventSink = events;
-                    }
+        EventChannel eventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), streamName);
+        eventChannel.setStreamHandler(new EventChannel.StreamHandler() {
+            @Override
+            public void onListen(Object args, final EventChannel.EventSink events) {
+                eventSink = events;
+            }
 
-                    @Override
-                    public void onCancel(Object args) {
-//                Log.w(TAG, "cancelling listener");
-                    }
-                }
-        );
-    }
-
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_payment_sdk_bridge_emulator");
-        channel.setMethodCallHandler(new FlutterPaymentSdkBridgePlugin());
+            @Override
+            public void onCancel(Object args) {
+            }
+        });
     }
 
     @Override
